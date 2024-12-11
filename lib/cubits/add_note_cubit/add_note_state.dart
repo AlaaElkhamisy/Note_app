952eb805e9a103d72cfbccf52041adb464e2a1d5
AlaaElkhamisy <wkood1108@gmail.com>
2024-12-11 19:05:26 +0200

    add note state

---
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -0,0 +1,20 @@
+part of 'add_note_cubit.dart';
+
+sealed class AddNoteState extends Equatable {
+  const AddNoteState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AddNoteInitial extends AddNoteState {}
+
+class AddNoteLoading extends AddNoteState {}
+
+class AddNoteSuccess extends AddNoteState {}
+
+class AddNoteFailure extends AddNoteState {
+  final String errMessage;
+
+  const AddNoteFailure(this.errMessage);
+}
--- lib/view/widgets/add_note_bottom_sheet.dart
+++ lib/view/widgets/add_note_bottom_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:note/customs/custom_button.dart';
-import 'package:note/customs/custom_text_field.dart';
+import 'package:note/view/widgets/add_note_form.dart';
 
 class Add_Note_Bottom_Sheet extends StatelessWidget {
   const Add_Note_Bottom_Sheet({super.key});
@@ -11,67 +10,6 @@ class Add_Note_Bottom_Sheet extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
         child: AddNoteForm(),
-      ),
-    );
-  }
-}
-
-class AddNoteForm extends StatefulWidget {
-  const AddNoteForm({
-    super.key,
-  });
-
-  @override
-  State<AddNoteForm> createState() => _AddNoteFormState();
-}
-
-class _AddNoteFormState extends State<AddNoteForm> {
-  final GlobalKey<FormState> formKey = GlobalKey();
-  // used to return error message when the user enter an erro input or epmty
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-  String? title, content;
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 35,
-          ),
-          CustomTextField(
-            onSaved: (value) {
-              title = value;
-            },
-            hint: "Title",
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          CustomTextField(
-            onSaved: (value) {
-              content = value;
-            },
-            hint: "Content",
-            maxLines: 5,
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          CustomButton(
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-                setState(() {});
-              }
-            },
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-        ],
       ),
     );
   }
